353e63a1096689fa79eafa0f03619db59a53cd6a
sweta mishra <sweta8indore@gmail.com>
2025-12-05 15:50:24 +0530

    CHANGE backend url2

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { Button, TextField, Container, Typography, Grid, Card, CardContent, Icon
 import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
-const API_BASE_URL = 'http://4.213.17.201:8000';
+const API_BASE_URL = 'http://4.213.17.201:8000/api'
 
 const backgroundImage = process.env.PUBLIC_URL + '/background.jpg';
 
